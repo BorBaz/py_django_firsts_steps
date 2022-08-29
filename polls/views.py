c6bb3078ff8129f87565f270c03524160451590b
Borja <boorjabs@gmail.com>
2022-08-29 22:06:43 +0200

    First view

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    return HttpResponse("Hola, mundo. Estás en la app de polls (no me acuerdo como se dice polls en ingles)")
