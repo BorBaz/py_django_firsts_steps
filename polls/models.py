887225355b4c2ad7d752790658129a1f8799acb3
Borja <boorjabs@gmail.com>
2022-08-30 21:20:54 +0200

    Added toString() -hehe- methods in our model in order to display readable information in $ python manage.py shell

--- polls/models.py
+++ polls/models.py
@@ -6,8 +6,14 @@ class Question(models.Model):
     question_text = models.CharField('question', max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return self.question_text
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField('choice', max_length=200)
     votes = models.IntegerField('votes', default=0)
+
+    def __str__(self):
+        return self.choice_text
